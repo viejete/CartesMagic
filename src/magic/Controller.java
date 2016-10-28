2e09d8f8c0a308bfe9ee48c52efdb12b9a2f3632
Ramon Casamajó <rcasamajo@iespoblenou.org>
2016-10-28 11:56:28 +0200

    Listener

--- src/magic/Controller.java
+++ src/magic/Controller.java
@@ -1,11 +1,25 @@
 package magic;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 
 public class Controller {
     public AnchorPane apMainPane;
+    public ListView<String> lvLlistaCartes;
 
     public void initialize(){
+        ObservableList<String> names = FXCollections.observableArrayList(
+                "Julia", "Ian", "Sue", "Matthew", "Hannah", "Stephan", "Denise");
+        lvLlistaCartes.setItems(names);
+
+        // Handle ListView selection changes.
+        lvLlistaCartes.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> {
+                System.out.println("ListView Selection Changed (selected: " + newValue.toString() + ")");
+            }
+        );
     }
+
 }
