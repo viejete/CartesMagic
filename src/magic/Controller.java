e338cb2eba7a9f03006eaaac565e7f00d0f17999
Ramon Casamajó <rcasamajo@iespoblenou.org>
2016-11-07 01:59:56 +0100

    ListView amb icones

--- src/magic/Controller.java
+++ src/magic/Controller.java
@@ -2,22 +2,42 @@ package magic;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
 
 public class Controller {
     public AnchorPane apMainPane;
     public ListView<String> lvLlistaCartes;
 
     public void initialize(){
+
+        // Personalitzem la CellFactory
+        lvLlistaCartes.setCellFactory((list) -> {
+            return new ListCell<String>() {
+                @Override
+                public void updateItem(String item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (empty) {
+                        setGraphic(null);
+                    } else {
+                        setGraphic(new ImageView("icon.png"));
+                        setText(item);
+                    }
+                }
+            };
+        });
+
+        // Afegir llista observable d'items
         ObservableList<String> names = FXCollections.observableArrayList(
                 "Julia", "Ian");
         lvLlistaCartes.setItems(names);
 
-        // Handle ListView selection changes.
+        // Afegir un item
+        lvLlistaCartes.getItems().add("Pepi");
+
+        // Handle ListView selection changes with a listener
         lvLlistaCartes.getSelectionModel().selectedItemProperty().addListener(
                 (observable, oldValue, newValue) -> {
                 System.out.println("ListView Selection Changed (selected: " + newValue.toString() + ")");
