e3666f684eef05a8bec6d09196246870d3637a11
Ramon Casamajó <rcasamajo@iespoblenou.org>
2016-11-07 02:12:41 +0100

    ListView amb icones

--- src/magic/Controller.java
+++ src/magic/Controller.java
@@ -9,40 +9,59 @@ import javafx.scene.layout.AnchorPane;
 
 public class Controller {
     public AnchorPane apMainPane;
-    public ListView<String> lvLlistaCartes;
+    public ListView<Person> lvLlistaCartes;
 
     public void initialize(){
 
         // Personalitzem la CellFactory
         lvLlistaCartes.setCellFactory((list) -> {
-            return new ListCell<String>() {
+            return new ListCell<Person>() {
                 @Override
-                public void updateItem(String item, boolean empty) {
+                public void updateItem(Person item, boolean empty) {
                     super.updateItem(item, empty);
                     if (empty) {
                         setGraphic(null);
                     } else {
-                        setGraphic(new ImageView("icon.png"));
-                        setText(item);
+                        setGraphic(new ImageView(item.getImg()));
+                        setText(item.getName());
                     }
                 }
             };
         });
 
         // Afegir llista observable d'items
-        ObservableList<String> names = FXCollections.observableArrayList(
-                "Julia", "Ian");
-        lvLlistaCartes.setItems(names);
+        ObservableList<Person> persons = FXCollections.observableArrayList(
+                new Person("Julia", "icon.png"), new Person("Greta", "icon.png"));
+        lvLlistaCartes.setItems(persons);
 
         // Afegir un item
-        lvLlistaCartes.getItems().add("Pepi");
+        lvLlistaCartes.getItems().add(new Person("Pepi", "icon.png"));
 
         // Handle ListView selection changes with a listener
         lvLlistaCartes.getSelectionModel().selectedItemProperty().addListener(
                 (observable, oldValue, newValue) -> {
-                System.out.println("ListView Selection Changed (selected: " + newValue.toString() + ")");
+                System.out.println("ListView Selection Changed (selected: " + newValue.getName() + ")");
             }
         );
+    }
+
+
+    class Person {
+        String name;
+        String img;
+
+        public Person(String name, String img) {
+            this.name = name;
+            this.img = img;
+        }
+
+        public String getImg() {
+            return img;
+        }
+
+        public String getName() {
+            return name;
+        }
     }
 
 }
