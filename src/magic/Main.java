b6800da4e021b3f68549e03feff9531cab8f337f
viejete <53639858v@iespoblenou.org>
2016-11-18 17:54:07 +0100

    Començem

--- src/magic/Main.java
+++ src/magic/Main.java
@@ -12,8 +12,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("magic.fxml"));
         primaryStage.setTitle("Cartes Magic");
-        primaryStage.setScene(new Scene(root, 600, 400));
-        primaryStage.setMinHeight(400);
+        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.setMinHeight(600);
         primaryStage.setMinWidth(600);
         primaryStage.show();
     }
